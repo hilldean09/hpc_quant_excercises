8ccd64327b192db49cc9d2a63530da9c9f6ea0a7
Dean Hill <inkandflowers@proton.me>
2026-01-27 14:09:11 +0800

    Declare simulate_asset_price_walk

---
+++ src/mpi_monte_carlo_options_pricing/mpi_monte_options.hpp
@@ -0,0 +1,9 @@
+
+
+
+float* Simulate_Asset_Price_Walk( initial_price = MMCOP_DEFAULT_INITIAL_PRICE, 
+                                  exp_initial_deviation = MMCOP_DEFAULT_EXP_INITIAL_DEVIATION, 
+                                  mean = MMCOP_DEFAULT_MEAN, 
+                                  persistence = MMCOP_DEFAULT_PERSISTENCE,
+                                  volxtility = MMCOPD_DEFULT_VOLATILITY );
+
