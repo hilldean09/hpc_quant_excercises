388f7b659f345de56111166fb1a654c16820335d
Dean Hill <inkandflowers@proton.me>
2026-02-02 09:47:47 +0800

    Debug script_mpi_monte_options.py

---
+++ scripts/script_mpi_monte_options.py
@@ -0,0 +1,20 @@
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Read_NPY( npy_filename ):
+    price_paths_array = np.load( npy_filename )
+
+    return np.transpose( price_paths_array )
+
+def Generate_Price_Paths_Plot( price_paths_array ):
+    for run in range( 0,  int( price_paths_array.shape[ 0 ] ) ):
+        plt.plot( price_paths_array[ run ][ : ] )
+
+
+price_paths_array = Read_NPY( sys.argv[ 1 ] )
+
+Generate_Price_Paths_Plot( price_paths_array )
+
+plt.show()
