69228323af8244e2f83d00a12c84668350d23dc2
Dean Hill <inkandflowers@proton.me>
2026-02-06 21:13:37 +0800

    Write benchmarking program using Google Benchmark

---
+++ src/mpi_monte_carlo_options_pricing/benchmark_mpi_monte_options.cpp
@@ -0,0 +1,57 @@
+
+
+
+#include "./pre_controls.hpp"
+#include "./mpi_monte_options.hpp"
+
+#include <benchmark/benchmark.h>
+
+#include <iostream>
+#include <string>
+
+
+using namespace MPI_MONTE_OPTIONS;
+
+
+#define MMCOP_BM_TOTAL_RUNS 100
+#define MMCOP_BM_TOTAL_TIMESTEPS 504
+
+
+static void BM_Run_Single_Threaded_Simulation( benchmark::State& state ) {
+  // Parameters
+  unsigned long long total_runs = MMCOP_BM_TOTAL_RUNS;
+  unsigned long long total_timesteps = MMCOP_BM_TOTAL_TIMESTEPS;
+  unsigned long long seed = MMCOP_DEFAULT_SEED;
+  bool do_write_to_file = true;
+  Heston_Parameters parameters = Construct_Parameters_Object( MMCOP_DEFAULT_INITIAL_PRICE, MMCOP_DEFAULT_INITIAL_VARIANCE, MMCOP_DEFAULT_TIMESTEP, MMCOP_DEFAULT_DRIFT, MMCOP_DEFAULT_MEAN_REVERSION_SPEED, MMCOP_DEFAULT_MEAN_REVERSION_LEVEL, MMCOP_DEFAULT_VOLATILITY, MMCOP_DEFAULT_CORRELATION_FACTOR ); 
+  float strike_price = MMCOP_DEFAULT_STRIKE_PRICE;
+  float discounting_rate = MMCOP_DEFAULT_DISCOUNTING_RATE;
+
+  for( auto _ : state ) {
+    Run_Single_Threaded_Simulation( total_runs, total_timesteps, seed, do_write_to_file, parameters );
+  }
+}
+
+BENCHMARK( BM_Run_Single_Threaded_Simulation );
+
+
+static void BM_Run_Multi_Threaded_Simulation( benchmark::State& state ) {
+  // Parameters
+  unsigned long long total_runs = MMCOP_BM_TOTAL_RUNS;
+  unsigned long long total_timesteps = MMCOP_BM_TOTAL_TIMESTEPS;
+  unsigned long long seed = MMCOP_DEFAULT_SEED;
+  bool do_write_to_file = true;
+  Heston_Parameters parameters = Construct_Parameters_Object( MMCOP_DEFAULT_INITIAL_PRICE, MMCOP_DEFAULT_INITIAL_VARIANCE, MMCOP_DEFAULT_TIMESTEP, MMCOP_DEFAULT_DRIFT, MMCOP_DEFAULT_MEAN_REVERSION_SPEED, MMCOP_DEFAULT_MEAN_REVERSION_LEVEL, MMCOP_DEFAULT_VOLATILITY, MMCOP_DEFAULT_CORRELATION_FACTOR ); 
+  float strike_price = MMCOP_DEFAULT_STRIKE_PRICE;
+  float discounting_rate = MMCOP_DEFAULT_DISCOUNTING_RATE;
+
+  for( auto _ : state ) {
+    Run_Multi_Threaded_Simulation( total_runs, total_timesteps, seed, do_write_to_file, parameters );
+  }
+}
+
+BENCHMARK( BM_Run_Multi_Threaded_Simulation );
+
+
+BENCHMARK_MAIN();
+
