1c0d516ad47261df26d53d5905e052db771ac932
Dean Hill <inkandflowers@proton.me>
2026-02-22 15:04:18 +0800

    Improving benchmarking program to test a range of values

--- docs/Excercise_Details.md
+++
@@ -1,4 +0,0 @@
-
-# Exercise Details
-
-This contains extra details as to the usage, functionality, notable aspects, and what was learned in regards to these exercises.
--- src/mpi_monte_carlo_options_pricing/benchmark_mpi_monte_options.cpp
+++ src/mpi_monte_carlo_options_pricing/benchmark_mpi_monte_options.cpp
@@ -19,35 +19,32 @@ using namespace MPI_MONTE_OPTIONS;
 
 static void BM_Run_Single_Threaded_Simulation( benchmark::State& state ) {
   // Parameters
-  unsigned long long total_runs = MMCOP_BM_TOTAL_RUNS;
   unsigned long long total_timesteps = MMCOP_BM_TOTAL_TIMESTEPS;
   unsigned long long seed = MMCOP_DEFAULT_SEED;
   bool do_write_to_file = false;
   Heston_Parameters parameters = Construct_Parameters_Object( MMCOP_DEFAULT_INITIAL_PRICE, MMCOP_DEFAULT_INITIAL_VARIANCE, MMCOP_DEFAULT_TIMESTEP, MMCOP_DEFAULT_DRIFT, MMCOP_DEFAULT_MEAN_REVERSION_SPEED, MMCOP_DEFAULT_MEAN_REVERSION_LEVEL, MMCOP_DEFAULT_VOLATILITY, MMCOP_DEFAULT_CORRELATION_FACTOR ); 
 
   for( auto _ : state ) {
-    Run_Single_Threaded_Simulation( total_runs, total_timesteps, seed, do_write_to_file, parameters );
+    Run_Single_Threaded_Simulation( state.range( 0 ), total_timesteps, seed, do_write_to_file, parameters );
   }
 }
 
-BENCHMARK( BM_Run_Single_Threaded_Simulation );
+BENCHMARK( BM_Run_Single_Threaded_Simulation )->RangeMultiplier( 2 )->Range( 64, 1'000'000 );
 
 
 static void BM_Run_Multi_Threaded_Simulation( benchmark::State& state ) {
   // Parameters
-  unsigned long long total_runs = MMCOP_BM_TOTAL_RUNS;
   unsigned long long total_timesteps = MMCOP_BM_TOTAL_TIMESTEPS;
   unsigned long long seed = MMCOP_DEFAULT_SEED;
   bool do_write_to_file = false;
   Heston_Parameters parameters = Construct_Parameters_Object( MMCOP_DEFAULT_INITIAL_PRICE, MMCOP_DEFAULT_INITIAL_VARIANCE, MMCOP_DEFAULT_TIMESTEP, MMCOP_DEFAULT_DRIFT, MMCOP_DEFAULT_MEAN_REVERSION_SPEED, MMCOP_DEFAULT_MEAN_REVERSION_LEVEL, MMCOP_DEFAULT_VOLATILITY, MMCOP_DEFAULT_CORRELATION_FACTOR ); 
 
   for( auto _ : state ) {
-    Run_Multi_Threaded_Simulation( total_runs, total_timesteps, seed, do_write_to_file, parameters );
+    Run_Multi_Threaded_Simulation( state.range( 0 ), total_timesteps, seed, do_write_to_file, parameters );
   }
 }
 
-BENCHMARK( BM_Run_Multi_Threaded_Simulation );
-
+BENCHMARK( BM_Run_Multi_Threaded_Simulation )->RangeMultiplier( 2 )->Range( 64, 1'000'000 );
 
 BENCHMARK_MAIN();
 
